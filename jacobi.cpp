3baaea68cb6ded726edf3846b5a828ba7fb2f32f
Tyft5 <ttippens6@gatech.edu>
2017-04-23 14:44:28 -0400

    Implemented serial jacobi function

--- jacobi.cpp
+++ jacobi.cpp
@@ -32,5 +32,41 @@ void matrix_vector_mult(const int n, const int m, const double* A, const double*
 // implements the sequential jacobi method
 void jacobi(const int n, double* A, double* b, double* x, int max_iter, double l2_termination)
 {
-    // TODO
+    // Initialize x, find the diagonal of A, copy A to R with diagonal set to 0
+    double *diag = (double *) malloc(n * sizeof(double));
+    double *R = (double *) malloc(n * n * sizeof(double));
+    double *w = (double *) malloc(n * sizeof(double));
+
+    for (int i = 0; i < n; i++) {
+        x[i] = 0;
+        diag[i] = A[i * n + i];
+        for (int j = 0; j < n; j++) {
+            if (i == j) { R[i * n + j] = 0; }
+            else { R[i * n + j] = A[i * n + j]; }
+        }
+    }
+
+    int iter = 0;
+    double l2, sum = 0;
+    while (iter < max_iter) {
+        // Perform Jacobi's method
+        matrix_vector_mult(n, R, x, w);
+        for (int i = 0; i < n; i++) {
+            x[i] = (b[i] - w[i]) / diag[i];
+        }
+
+        // Check for convergence
+        matrix_vector_mult(n, A, x, w);
+        for (int i = 0; i < n; i++) {
+            sum += pow(w[i] - b[i], 2);
+        }
+        l2 = sqrt(sum);
+        if (l2 <= l2_termination) break;
+
+        iter++
+    }
+
+    free(R);
+    free(diag);
+    free(w);
 }
